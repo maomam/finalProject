55633d4d449d711fdd72408320c7c79e2a7a5191
maomam <daryna.ariamnova@gmail.com>
2023-02-25 10:41:06 +0100

    vibration correction 2

--- js/relax.js
+++ js/relax.js
@@ -314,7 +314,7 @@ function draw() {
         if (playing) {
                 elements.map(shape);
                 let prop = elements[10].getBoundingClientRect();
-                let vib = map(prop.bottom, 150, window.height-150, 0, 100)
+                let vib = map(prop.bottom, 150, window.height-150, 0, 300)
                 navigator.vibrate(vib); 
                 let anim = elements[10].getAnimations();
                 let curr; 
