b34dca881e80ec9bcea76b6c14db1534a7f8829b
maomam <daryna.ariamnova@gmail.com>
2023-02-25 10:46:45 +0100

    vigrate correction 3

--- js/relax.js
+++ js/relax.js
@@ -314,7 +314,7 @@ function draw() {
         if (playing) {
                 elements.map(shape);
                 let prop = elements[10].getBoundingClientRect();
-                let vib = map(prop.bottom, 150, window.height-150, 0, 300)
+                let vib = map(prop.bottom, 150, window.height-150, 50, 350)
                 navigator.vibrate(vib); 
                 let anim = elements[10].getAnimations();
                 let curr; 
